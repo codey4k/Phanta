dc87f0d60f5752a0e97cba287284e827c5c07853
codey4k <codey4k@gmail.com>
2018-03-25 23:26:34 +0000

    adding hero images to header

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ get_header();
 $header = '
     <header class="header">
         <div class="header__container">
-            <img src="" alt="header img" class="header__img" />
+            <img src="%s/images/hero.jpg" alt="header img" class="header__img" />
         </div>
     </header>
     <main class="main">
@@ -13,6 +13,6 @@ $header = '
     </main>
 ';
 
-printf($header);
+printf($header, get_stylesheet_directory_uri());
 
 get_footer();
