750c494b775844737b4912478833d262ad60f5c3
codey4k <codey4k@gmail.com>
2018-04-04 18:05:44 +0000

    redesign menu section

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    window.addEventListener('scroll', ()=>{
-        let scroll = document.documentElement.scrollTop || document.body.scrollTop,
-            menu = document.getElementById('menu-bar')
+    // window.addEventListener('scroll', ()=>{
+    //     let scroll = document.documentElement.scrollTop || document.body.scrollTop,
+    //         menu = document.getElementById('menu-bar')
             
             
-        if(scroll >= 50) {
-            Object.assign(menu.style, {
-                height : '60px',
-                background : '#444'
-            })
-        }
-        else {
-            Object.assign(menu.style, {
-                height : '80px',
-                background : 'transparent'
-            })
-        }
-    })
+    //     if(scroll >= 50) {
+    //         Object.assign(menu.style, {
+    //             height : '60px',
+    //             background : '#444'
+    //         })
+    //     }
+    //     else {
+    //         Object.assign(menu.style, {
+    //             height : '80px',
+    //             background : 'transparent'
+    //         })
+    //     }
+    // })
 })
